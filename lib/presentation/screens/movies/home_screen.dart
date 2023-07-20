e45f5d7a3f44de64b19972a68e349ffb2452f479
drakkomaximo <drakkomaximo1234@gmail.com>
2023-07-20 14:55:03 -0500

    Full Screen Loader has been integrated with cinemapedia app

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -36,13 +36,15 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+
+    final initialLoading = ref.watch( initialLoadingProvider );
+    if( initialLoading ) return const FullScreenLoader();
+
     final slideshowMovies = ref.watch(moviesSlideshowProvider);
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
     final upComingMovies = ref.watch(upComingMoviesProvider);
-
-    return const FullScreenLoader();
 
     return CustomScrollView(slivers: [
       const SliverAppBar(
--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -61,7 +61,7 @@ class _MovieHorizontalListviewState extends State<MovieHorizontalListview> {
                 scrollDirection: Axis.horizontal,
                 physics: const BouncingScrollPhysics(),
                 itemBuilder: (context, index) =>
-                    _Slide(movie: widget.movies[index])))
+                    FadeInRight(child: _Slide(movie: widget.movies[index]))))
       ]),
     );
   }
