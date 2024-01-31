1afc2a827b82b4c1297184124fde598750b38a1d
drakkomaximo <drakkomaximo1234@gmail.com>
2024-01-31 16:02:04 -0500

    first GoRouter part

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,11 +1,40 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/home_views/favorites_view.dart';
+import 'package:cinemapedia/presentation/views/home_views/home_view.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
+                  return MovieScreen(moviedId: movieId);
+                },
+              )
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        )
+      ])
+  /* GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const HomeScreen( childView: FavoritesView(),),
       //rutas hijas
       routes: [
         GoRoute(
@@ -16,5 +45,5 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
             return MovieScreen(moviedId: movieId);
           },
         )
-      ]),
+      ]), */
 ]);
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,97 +1,18 @@
-import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
-  const HomeScreen({super.key});
+
+  final Widget childView;
+
+  const HomeScreen({super.key, required this.childView});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigationBar(),
+    return Scaffold(
+      body: childView,
+      bottomNavigationBar: const CustomBottomNavigationBar(),
     );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-    ref.read(upComingMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    final initialLoading = ref.watch( initialLoadingProvider );
-    if( initialLoading ) return const FullScreenLoader();
-
-    final slideshowMovies = ref.watch(moviesSlideshowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    final upComingMovies = ref.watch(upComingMoviesProvider);
-
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        floating: true,
-        flexibleSpace: FlexibleSpaceBar(
-          title: CustomAppBar(),
-          titlePadding: EdgeInsets.zero,
-          centerTitle: false,
-        ),
-      ),
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-            MoviesSlideshow(movies: slideshowMovies),
-            MovieHorizontalListview(
-              movies: nowPlayingMovies,
-              title: 'En cines',
-              subTitle: 'Lunes 20',
-              loadNextPage: () =>
-                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
-            ),
-            MovieHorizontalListview(
-              movies: upComingMovies,
-              title: 'Proximamente',
-              subTitle: 'En este mes',
-              loadNextPage: () =>
-                  ref.read(upComingMoviesProvider.notifier).loadNextPage(),
-            ),
-            MovieHorizontalListview(
-              movies: popularMovies,
-              title: 'Populares',
-              //subTitle: '',
-              loadNextPage: () =>
-                  ref.read(popularMoviesProvider.notifier).loadNextPage(),
-            ),
-            MovieHorizontalListview(
-              movies: topRatedMovies,
-              title: 'Mejor calificadas',
-              subTitle: 'Desde siempre',
-              loadNextPage: () =>
-                  ref.read(topRatedMoviesProvider.notifier).loadNextPage(),
-            ),
-            const SizedBox(height: 10)
-          ],
-        );
-      }, childCount: 1))
-    ]);
   }
 }
